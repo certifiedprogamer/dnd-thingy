bcdbf5dd2051ccaa41f7455d5ed3ec7f56c41958
Kerry Sowers <sowerskerry2@gmail.com>
2026-01-22 08:43:23 -0500

    added defer

--- idk.js
+++ idk.js
@@ -1,12 +1,16 @@
 
 function g(){
-    fetch("https://www.dnd5eapi.co/api/2014/spells/acid-arrow")
+    
+    let d = fetch("https://www.dnd5eapi.co/api/2014/spells/acid-arrow")
           .then((response) => {
             return response.json();
           }).then(data => {
             console.log(data)
+            let m = data
+            return m
           })
-        }
+    return d 
+}
 
-
-g()
+const c = g()
+console.log(c)
